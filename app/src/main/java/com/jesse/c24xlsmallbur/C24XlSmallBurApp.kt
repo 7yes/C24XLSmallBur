baf87eb2c0d199b5e4b4969c536efa52700bcea1
yesmc <7yesid@gmail.com>
2024-11-23 09:53:46 -0500

    ui and navigation

---
+++ app/src/main/java/com/jesse/c24xlsmallbur/C24XlSmallBurApp.kt
@@ -0,0 +1,7 @@
+package com.jesse.c24xlsmallbur
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class C24XlSmallBurApp:Application()
---
+++ app/src/main/java/com/jesse/c24xlsmallbur/presentation/ui/HomeFragment.kt
@@ -0,0 +1,31 @@
+package com.jesse.c24xlsmallbur.presentation.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jesse.c24xlsmallbur.R
+import com.jesse.c24xlsmallbur.databinding.FragmentDetailBinding
+import com.jesse.c24xlsmallbur.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
